7153cb3990785c90b3360c7c8080fdf5213fe41c
Stavrina Sofianopoulou <s.sofianopoulou@stoiximan.gr>
2018-02-21 11:31:03 +0200

    new dependancy

--- lib/cli.ex
+++ lib/cli.ex
@@ -4,17 +4,38 @@ defmodule Proj_issues.CLI do
       """
     @default_count 4
     def run(argv) do
-        parse_args(argv)
+        argv
+        |> parse_args
+        |>process
     end
 
       
     def parse_args(argv) do
-      parse = OptionParser.parse(argv ,switches: [help: :boolean] ,aliases: [h: :help])
-      case parse do 
-        {[help: true],_,_} -> :help
-        {_,[user,project,count],_} -> {user,project,String.to_integer(count)}
-        {_,[user,project],_} -> {user,project,@default_count}
-        _ -> :help
-      end
+      OptionParser.parse(argv ,switches: [help: :boolean] ,aliases: [h: :help])
+      |>elem(1)
+      |>args_to_internal_rep()
+    end
+
+    def args_to_internal_rep([user,project,count]) do
+        {user,project,String.to_integer(count)}
+    end
+
+    def args_to_internal_rep([user,project]) do
+        {user,project,@default_count}
+    end
+
+    def args_to_internal_rep(_) do
+        :help
+    end
+
+    def process(:help) do
+        IO.puts """
+        usage: issues <user> <project> [count | #{@default_count}]
+        """
+        System.halt(0)
+    end
+
+    def process({user,project,_count}) do
+        Proj_issues.Github.fetch(user,project)
     end
 end
