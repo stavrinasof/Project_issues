05177fd6a9dcbf602322c75336daec2c77649c94
Stavrina Sofianopoulou <s.sofianopoulou@stoiximan.gr>
2018-02-21 10:59:13 +0200

    passed initial tests

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,13 +1,20 @@
 defmodule Proj_issues.CLI do
+    @doc """
+      'argv' can be -h or --help ,which returns :help
+      """
+    @default_count 4
     def run(argv) do
         parse_args(argv)
     end
 
+      
     def parse_args(argv) do
-        parse = is_list(argv)
-        case parse do
-            true -> "you passed a list"
-            false -> "expected a list"
-        end
+      parse = OptionParser.parse(argv ,switches: [help: :boolean] ,aliases: [h: :help])
+      case parse do 
+        {[help: true],_,_} -> :help
+        {_,[user,project,count],_} -> {user,project,String.to_integer(count)}
+        {_,[user,project],_} -> {user,project,@default_count}
+        _ -> :help
+      end
     end
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -3,10 +3,18 @@ defmodule CLITest do
    #doctest ProjIssues
   import Proj_issues.CLI ,only: [parse_args: 1]
 
-  test ":working" do
-    assert parse_args([]) == "you passed a list"
-    assert parse_args(1) != "you passed a list"
+   #test ":failing" ,do:    assert parse_args(1) == "you passed a list"
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
   end
 
-  #test ":failing" ,do:    assert parse_args(1) == "you passed a list"
+  test "count is defaulted if two values given" do
+    assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+
+  test "three values returned if three given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+    end
+ 
 end
